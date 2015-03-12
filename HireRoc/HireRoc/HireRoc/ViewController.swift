1d89d1a49e17d19f4f6b0d875fced69049abe1ec
Jacob Niebloom <niebloomj@gmail.com>
2015-03-12 16:05:10 -0400

    Create HireRoc

---
+++ HireRoc/HireRoc/HireRoc/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  HireRoc
+//
+//  Created by Jacob Niebloom on 3/12/15.
+//  Copyright (c) 2015 Phatapps Development, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	@IBAction func ButtonPressed(sender: AnyObject) {
+		println("Hello world")
+	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
